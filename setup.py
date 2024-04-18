f90a29e0663e0af6622664cce241db92657885c4
unknown <ashwinmurali27@gmail.com>
2024-04-18 18:09:10 +0530

    resolved problem with creating config file in setup.py

--- setup.py
+++ setup.py
@@ -8,31 +8,24 @@ os.system("cls")
 
 content = f'TOKEN = "{token}"\nMODEL = "{model}"'
 
-with open('data_extractor/API_CONFIG.py', 'w') as f:
+with open('backend/data_extractor/API_CONFIG.py', 'w') as f:
     f.write(content)
     
 print("API config file created successfully")
-time.sleep(1)
+time.sleep(2)
 os.system("cls")
 
 req = input("Do you want to install the dependencies now? (y/n): ")
 if req.lower() == "y":
     os.system("pip install -r requirements.txt")
     time.sleep(2)
-    os.system("cls")
     print("if you encounter any error, please install the dependencies manually by running 'pip install -r requirements.txt' or by installing the requirements one by one using 'pip install <package_name>'")
     time.sleep(5)
 else:
     print("Please install the dependencies by running 'pip install -r requirements.txt' or by installing the requirements one by one using 'pip install <package_name>'")
-    time.sleep(1)
-    os.system("cls")
-
-print("Setup completed")
-time.sleep(1)
-os.system("cls")
+    time.sleep(2)
 
 print("Make sure you have installed the Tesseract OCR and added it to the PATH variable")
 time.sleep(1)
 print("You can now run the program by running 'backend.py' inside backend folder")
 time.sleep(5)
-exit()
